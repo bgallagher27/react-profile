b67507f31dc40916159388d21ea55c08aaff3050
Brendan Gallagher <bpgallagher27@gmail.com>
2020-05-16 16:16:24 -0400

    header initialized

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Header(props) {
+    return (
+        <header className="header">
+            <span className="name">
+                Brendan Gallagher
+            </span>
+
+            <span className="navigation">
+                <nav>
+                    <Link to="/">
+                        About Me
+                    </Link>
+                    <Link to="/portfolio">
+                        Portfolio
+                    </Link>
+                    <Link to="/contact">
+                        Contact
+                    </Link>
+                </nav>
+            </span>
+        </header>
+    )
+};
+
+export default Header;
