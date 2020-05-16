f329d34e7e1c93f470ae3c493e7b36163284d973
Brendan Gallagher <bpgallagher27@gmail.com>
2020-05-16 16:05:39 -0400

    initial footer setup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Home from "./components/Home";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/Contact";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
       <Header />
-        <Router>
+        {/* <Router>
           <Route exact path="/" component={ Home } />
           <Route exact path="/portfolio" component={ Portfolio } />
           <Route exact path="/contact" component={ Contact } />
-        </Router>
+        </Router> */}
       <Footer />
     </div>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+    return (
+        <footer className="footer">
+            Copyright (c)
+        </footer>
+    )
+};
+
+export default Footer;
